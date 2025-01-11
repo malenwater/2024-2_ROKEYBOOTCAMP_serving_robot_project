cb366d252be6b2557ead87eff6ced5aebb1c6523
malenwater <128200788+malenwater@users.noreply.github.com>
2025-01-11 23:37:34 +0900

    Update kiosk_ui.py

--- src/serving_robot/serving_robot/kiosk/kiosk_ui.py
+++ src/serving_robot/serving_robot/kiosk/kiosk_ui.py
@@ -1,63 +1,57 @@
+import sys
+from PyQt5 import QtWidgets, uic
+
 class KioskDialog(QtWidgets.QDialog):
     def __init__(self):
         super().__init__()
         # ui 파일 로드
-        ui_file = "/home/gh/바탕화면/ROKEY_serving_robot_A-2/src/serving_robot/resource/ui/kiosk.ui"
+        ui_file = "../../resource/ui/kiosk.ui"
         uic.loadUi(ui_file, self)
-        menu_number = ["","짜장면","간짜장","쟁반짜장",
+        self.menu_number = ["","짜장면","간짜장","쟁반짜장",
                        "짬뽕","짬뽕밥","짜장밥",
                        "탕수육","깐풍기","군만두",
                        "팔보채","고추잡채","꽃 빵",
                        "사이다","콜라","환타",
                        "소주","맥주","고량주",]
-        menu_order = {}
+        self.menu_order = {}
         for i in range(1, 19):  # 1부터 18까지 반복
-            menu_order[menu_number[i]] = str(i)
+            self.menu_order[self.menu_number[i]] = str(i)
         # 위젯 딕셔너리 정의
         self.widgets = {
-            "foodButton": self.findChild(QtWidgets.QPushButton, "foodButton"),
-            "sidefoodButton": self.findChild(QtWidgets.QPushButton, "sidefoodButton"),
-            "drinkButton": self.findChild(QtWidgets.QPushButton, "drinkButton"),
             # 주문하기 18개 위젯 만들어서 1개의 함수로 통일 시키기
             # 결제하기 함수 1개
             # -, + 이 둘도 하나의 함수로 만들기
             # 라벨 음식갯수 라벨, 총 가격과 메뉴별 가격 라벨 위젯
-            "orders_layout" : self.findChild(QtWidgets.QFormLayout, "orders_layout")
-            # selectButton_1 부터 selectButton_18까지 정의
-            **{f"selectButton_{i}": self.findChild(QtWidgets.QPushButton, f"selectButton_{i}") for i in range(1, 19)},
-            # 장바구니 개수 증가/감소 버튼
-            **{f"plusCountButton_{i}": self.findChild(QtWidgets.QPushButton, f"plusCountButton_{i}") for i in range(1, 19)},
-            **{f"minusCountButton_{i}": self.findChild(QtWidgets.QPushButton, f"minusCountButton_{i}") for i in range(1, 19)},
-
+            "foodButton": self.findChild(QtWidgets.QPushButton, "foodButton"),
+            "sidefoodButton": self.findChild(QtWidgets.QPushButton, "sidefoodButton"),
+            "drinkButton": self.findChild(QtWidgets.QPushButton, "drinkButton"),
+            "orders_layout" : self.findChild(QtWidgets.QFormLayout, "orders_layout"),
+            "orderButton" : self.findChild(QtWidgets.QPushButton, "orderButton"),
         }
         
-        for i in range(1, 19):  # 1부터 18까지 반복
-            widget_name = "order_" + menu_order[menu_number[i]]
-            self.widgets[widget_name] = self.findChild(QtWidgets.QFrame, widget_name)
-            self.widgets["orders_layout"].removeWidget(self.widgets[widget_name]) 
-            self.widgets[widget_name].setVisible(False)
-
-        self.widgets["order_1"].setVisible(True)
-        self.widgets["order_5"].setVisible(True)
-        self.widgets["order_10"].setVisible(True)
-        self.widgets["order_18"].setVisible(True)
-        self.widgets["order_2"].setVisible(True)
-        self.widgets["orders_layout"].addWidget(self.widgets["order_1"]) 
-        self.widgets["orders_layout"].addWidget(self.widgets["order_5"]) 
-        self.widgets["orders_layout"].addWidget(self.widgets["order_10"]) 
-        self.widgets["orders_layout"].addWidget(self.widgets["order_18"]) 
-        self.widgets["orders_layout"].addWidget(self.widgets["order_2"]) 
+        for i in range(1, 19):
+            order_name = self.__make_name_widgets("order_",self.menu_order[self.menu_number[i]])
+            select_name = self.__make_name_widgets("selectButton_",self.menu_order[self.menu_number[i]])
+            plusCount_name = self.__make_name_widgets("plusCountButton_",self.menu_order[self.menu_number[i]])
+            minusCount_name = self.__make_name_widgets("minusCountButton_",self.menu_order[self.menu_number[i]])
+            price_name = self.__make_name_widgets("price_",self.menu_order[self.menu_number[i]])
+            self.widgets[select_name] = self.findChild(QtWidgets.QPushButton, f"selectButton_{i}")
+            self.widgets[plusCount_name] = self.findChild(QtWidgets.QPushButton, f"plusCountButton_{i}")
+            self.widgets[minusCount_name] = self.findChild(QtWidgets.QPushButton, f"minusCountButton_{i}")
+            self.widgets[order_name] = self.findChild(QtWidgets.QFrame, order_name)
+            self.widgets[price_name] = self.findChild(QtWidgets.QLabel, price_name)
+            self.widgets[plusCount_name].clicked.connect(lambda _, x=i: self.change_quantity(x, 1))
+            self.widgets[minusCount_name].clicked.connect(lambda _, x=i: self.change_quantity(x, -1))
+            self.widgets["orders_layout"].removeWidget(self.widgets[order_name]) 
+            self.widgets[order_name].setVisible(False)
+            
+        self.widgets["foodButton"].clicked.connect(self.__on_scroll_move_button_click)
+        self.widgets["sidefoodButton"].clicked.connect(self.__on_scroll_move_button_click)
+        self.widgets["drinkButton"].clicked.connect(self.__on_scroll_move_button_click)
+        self.widgets["orderButton"].clicked.connect(self.handle_order)
+        # self.__order_menu_widget('order_10')
+        # self.__disorder_menu_widget('order_2')
         
-        # 버튼에 함수 연결
-        self.widgets["foodButton"].clicked.connect(self.on_scroll_move_button_click)
-        self.widgets["sidefoodButton"].clicked.connect(self.on_scroll_move_button_click)
-        self.widgets["drinkButton"].clicked.connect(self.on_scroll_move_button_click)
-       
-        # 결제하기 버튼
-        self.orderButton = self.findChild(QtWidgets.QPushButton, "orderButton")
-        if self.orderButton:
-            self.orderButton.clicked.connect(self.handle_order)
-
         # 장바구니와 관련된 라벨 정의
         self.menu_quantities = [0] * 18  # 각 메뉴의 수량
         self.menu_prices = [1000 + (i * 500) for i in range(18)]  # 각 메뉴의 가격 예시 (각 메뉴별로 다르게 설정 가능)
@@ -65,23 +59,18 @@ class KioskDialog(QtWidgets.QDialog):
 
         # 라벨 업데이트 함수
         self.update_labels()
-
-        # 버튼에 함수 연결
-        for i in range(1, 19):
-            self.widgets[f"plusCountButton_{i}"].clicked.connect(lambda _, x=i: self.change_quantity(x, 1))
-            self.widgets[f"minusCountButton_{i}"].clicked.connect(lambda _, x=i: self.change_quantity(x, -1))
-
-        # 메뉴판 상품 이름 및 가격 라벨 정의 (메뉴판)
-        self.menu_labels = {f"label_{i}": self.findChild(QtWidgets.QLabel, f"label_{i}") for i in range(18, 83, 3)}
-        self.menu_price_labels = {f"label_{i+1}": self.findChild(QtWidgets.QLabel, f"label_{i+1}") for i in range(18, 83, 3)}
-
-        # 해당 라벨이 없을 경우 기본값 설정
-        for i in range(18):
-            if not self.menu_labels.get(f"label_{i+18}"):
-                self.menu_labels[f"label_{i+18}"] = QtWidgets.QLabel(f"상품 {i+1}")
-            if not self.menu_price_labels.get(f"label_{i+19}"):
-                self.menu_price_labels[f"label_{i+19}"] = QtWidgets.QLabel(f"{self.menu_prices[i]}원")
-
+    def __order_menu_widget(self,menu_name):
+        self.widgets[menu_name].setVisible(True)
+        self.widgets["orders_layout"].addWidget(self.widgets[menu_name])
+    def __disorder_menu_widget(self,menu_name):
+        self.widgets[menu_name].setVisible(False)
+        self.widgets["orders_layout"].removeWidget(self.widgets[menu_name])
+    def __make_name_widgets(self, name, widget_number):
+        return name + widget_number
+    def __on_scroll_move_button_click(self):
+        # 버튼 누르면 특정위치 스크롤바 이동
+        pass
+    
     def change_quantity(self, menu_index, change):
         """특정 메뉴의 수량을 증가 또는 감소시킴"""
         # 수량이 0 이상일 때만 감소
